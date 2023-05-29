672eff8512137f9de485a95707e027903cecef5d
Bafmashina <108014263+Bafmashina@users.noreply.github.com>
2023-05-29 10:04:16 +0300

    Update add TotalSum in Redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
-import { useSelector } from 'react-redux'
-
+import { useSelector } from "react-redux";
 
 export const Header = () => {
   const location = useLocation();
-  const {items, totalPrice} = useSelector(state => state.cart)
-  
+  const { items, totalPrice } = useSelector((state) => state.cart);
 
   return (
     <header className="d-flex justify-between align-center p-25">
@@ -20,11 +18,14 @@ export const Header = () => {
 
       <ul className="d-flex">
         <li className="hederCart">
-          <img width={22} height={20} src="/img/like.png" alt="" />
+          <div className="LiekTotal">
+            <img width={22} height={20} src="/img/like.png" alt="" />
+            <span className="spanTotalPrice">{totalPrice}</span>
+          </div>
         </li>
         <Link to={`basket`}>
           {location.pathname !== `/basket` && (
-            <div>
+            <div className="totalItemsCountinCard">
               <img width={22} height={20} src="/img/Cart.png" alt="" />
               <span className="spanItemCount">{items.length}</span>
             </div>
