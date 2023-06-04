75e12d70213e875d491364ad6d2fae9f593eb06a
Bafmashina <108014263+Bafmashina@users.noreply.github.com>
2023-06-04 17:47:48 +0300

    Update Basket add TotalCountItems and debug countAllSum

--- src/components/BasketItem/BasketItem.jsx
+++ src/components/BasketItem/BasketItem.jsx
@@ -1,17 +1,22 @@
+import { clearItems} from "../../redux/slice/cartSlice";
 import { CartButton } from "./CartButton";
 import { CartItem } from "./CartItem";
 import "./basketItem.scss";
 import { useDispatch, useSelector } from "react-redux";
 
-export const BasketItem = () => {
+export const BasketItem = ({id}) => {
   const dispatch = useDispatch();
   const items = useSelector((state) => state.cart.items);
+
+  const onClickClear = () => {
+    dispatch(clearItems(id))
+  }
 
   return (
     <div className="body">
       <div className="wraperCart">
         <h1>Корзина</h1>
-        <div className="removeAllItems">
+        <div onClick={onClickClear} className="removeAllItems">
           <button className="removeAllItems-btn">Отчистить корзину</button>
           <img className="removeAllItems-img" src="img/basket/delete.png" alt="" />
         </div>
--- src/components/BasketItem/CartButton.jsx
+++ src/components/BasketItem/CartButton.jsx
@@ -1,14 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
+
 export const CartButton = () => {
   const { items, totalPrice } = useSelector((state) => state.cart);
+  const totalCount = items.reduce((sum, item) => sum + item.count, 0)
 
   return (
     <div className="priceComponent">
       <div className="contentPrice-C">
+        <div className="basketAllCounts">
+        <span>Кол-во товаров</span>
+          <b>{totalCount} шт</b>
+        </div>
         <div className="price">
-          <span>ИТОГО</span>
+          <span>Общая сумма</span>
           <b>{totalPrice} руб</b>
         </div>
         <div>
