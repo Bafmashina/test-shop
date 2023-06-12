5cf639fd35bbf540bb818228ed065f03755ff7d6
Bafmashina <108014263+Bafmashina@users.noreply.github.com>
2023-06-12 12:28:30 +0300

    Update Sort

--- src/components/Sort/Sort.jsx
+++ src/components/Sort/Sort.jsx
@@ -1,20 +1,37 @@
 import React from "react";
-import './sort.scss'
+import "./sort.scss";
 
 export const Sort = () => {
+  const [open, setOpen] = React.useState(false);
+  const [selected, setSelected] = React.useState(0)
+
+  const list = ["Популярности", "цене", "алфавиту"];
+  const sortName = list[selected]
+
+  const onClickSortItem = (id) => {
+    setSelected(id)
+    setOpen(false)
+  }
+
   return (
     <div className="wrapper-sort">
       <div className="sort">
         <b>Сортировать по: </b>
-        <span>Популярности</span>
+        <span onClick={() => setOpen(!open)}>{sortName}</span>
       </div>
-      <div className="sort-popup">
-        <ul>
-          <li className="active"> Популярности</li>
-          <li>цене</li>
-          <li>алфавиту</li>
-        </ul>
-      </div>
+      {open && (
+        <div className="sort-popup">
+          <ul>
+            {list.map((name, id) => (
+              <li
+                key={id}
+                onClick={() => onClickSortItem(id)}
+                className={selected === id ? "active" : ""}
+              >{name}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 };
