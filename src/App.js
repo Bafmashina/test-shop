79ea4dfaec15dd0e1e647cabdbd5c17d44f95f6e
Bafmashina <108014263+Bafmashina@users.noreply.github.com>
2023-06-18 21:00:21 +0300

    Add API in project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import {Home} from './components/Home/Home'
+import React from "react";
+import { Home } from "./components/Home/Home";
 
 function App() {
+
   return (
     <div className="wrapper clear">
-      <Home/>
+      <Home />
     </div>
   );
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,42 +3,52 @@ import React from "react";
 import { Header } from "../Header";
 import { Card } from "../Card/Card";
 import { Footer } from "../Footer";
-
-import hedphones from "../../assets/hedphones.json";
-import hedphones2 from "../../assets/hedphones2.json";
 import { Sort } from "../Sort/Sort";
 
 export const Home = () => {
+
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://648f2d9375a96b664444ca5f.mockapi.io/items")
+    .then((res) => {
+      return res.json();
+    })
+    .then((arr) => {
+      setItems(arr)
+    });
+  }, [])
+
+
   return (
     <>
       <Header />
       {/* ВВЕРХ */}
       <div className="content p-40">
-
         {/* Сортировка */}
-       <Sort/>
+        <Sort />
 
         <h1>Наушники</h1>
         <div className="d-flex mb-40">
-          {hedphones.map((el) => (
-            <Card key={el.id} {...el} />
+          {items.map((el, i) => ( i < 3 ?
+            <Card key={el.id} {...el} /> : ''
           ))}
         </div>
 
         {/* Вторая тройка */}
         <div className="secondItems">
           <div className="d-flex">
-            {hedphones.map((el) => (
-              <Card key={el.id} {...el} />
-            ))}
+          {items.map((el, i) => ( i >= 3 && i < 6 ?
+            <Card key={el.id} {...el} /> : ''
+          ))}
           </div>
         </div>
 
         {/* НИЗ */}
         <h1>Беспроводные Наушники</h1>
         <div className="d-flex mb-40">
-          {hedphones2.map((el) => (
-            <Card key={el.id} {...el} />
+        {items.map((el, i) => ( i >= 6 && i <= 9 ?
+            <Card key={el.id} {...el} /> : ''
           ))}
         </div>
       </div>
