ae61120be82447af239adbc5fb332e1050ba4812
Bafmashina <108014263+Bafmashina@users.noreply.github.com>
2023-06-19 11:49:01 +0300

    Update add Skeletons

---
+++ src/components/Card/Skeleton.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+const Skeleton = (props) => (
+  <ContentLoader 
+    speed={2}
+    width={400}
+    height={600}
+    viewBox="0 0 400 600"
+    backgroundColor="#decece"
+    foregroundColor="#efebeb"
+    {...props}
+  >
+    <rect x="24" y="23" rx="20" ry="20" width="360" height="352" /> 
+    <rect x="71" y="382" rx="20" ry="20" width="268" height="34" /> 
+    <rect x="28" y="424" rx="10" ry="10" width="102" height="25" /> 
+    <rect x="28" y="465" rx="10" ry="10" width="102" height="25" /> 
+    <rect x="281" y="424" rx="10" ry="10" width="102" height="25" /> 
+    <rect x="282" y="465" rx="10" ry="10" width="102" height="25" />
+  </ContentLoader>
+)
+
+export default Skeleton
+
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,21 +4,22 @@ import { Header } from "../Header";
 import { Card } from "../Card/Card";
 import { Footer } from "../Footer";
 import { Sort } from "../Sort/Sort";
+import Skeleton from "../Card/Skeleton";
 
 export const Home = () => {
-
   const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
 
   React.useEffect(() => {
     fetch("https://648f2d9375a96b664444ca5f.mockapi.io/items")
-    .then((res) => {
-      return res.json();
-    })
-    .then((arr) => {
-      setItems(arr)
-    });
-  }, [])
-
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        setItems(arr);
+        setIsLoading(false);
+      });
+  }, []);
 
   return (
     <>
@@ -30,26 +31,31 @@ export const Home = () => {
 
         <h1>Наушники</h1>
         <div className="d-flex mb-40">
-          {items.map((el, i) => ( i < 3 ?
-            <Card key={el.id} {...el} /> : ''
-          ))}
+          {isLoading
+            ? [...new Array(3)].map((_, index) => <Skeleton key={index} />)
+            : items.map((el, i) => (i < 3 ? <Card key={el.id} {...el} /> : ""))}
         </div>
 
         {/* Вторая тройка */}
         <div className="secondItems">
           <div className="d-flex">
-          {items.map((el, i) => ( i >= 3 && i < 6 ?
-            <Card key={el.id} {...el} /> : ''
-          ))}
+            {isLoading
+              ? [...new Array(3)].map((_, index) => <Skeleton key={index} />)
+              : items.map((el, i) =>
+                  i >= 3 && i < 6 ? <Card key={el.id} {...el} /> : ""
+                )}
           </div>
         </div>
 
         {/* НИЗ */}
         <h1>Беспроводные Наушники</h1>
         <div className="d-flex mb-40">
-        {items.map((el, i) => ( i >= 6 && i <= 9 ?
-            <Card key={el.id} {...el} /> : ''
-          ))}
+
+          {isLoading
+            ? [...new Array(3)].map((_, index) => <Skeleton key={index} />)
+            : items.map((el, i) =>
+                i >= 6 && i <= 9 ? <Card key={el.id} {...el} /> : ""
+              )}
         </div>
       </div>
       {/* Футер */}
